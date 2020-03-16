99154b14fb7414c1bfabd81b9082d3f5eafd8888
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-03-16 04:59:14 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -12,9 +12,9 @@ def find_max_value(array)
 
    array.length.times do |index|
      if array[index] > highest
-       highest = array[index]
+       highest = index
      end
-     highest 
+     highest
 end
 
 def find_min_value(array)
