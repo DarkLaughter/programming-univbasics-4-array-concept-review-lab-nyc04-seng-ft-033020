d41f884ed32c9e02d2edc1a09e1d59f7e3ef05fd
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-03-16 04:37:36 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,10 @@
 def find_element_index(array, value_to_find)
   length = array.size
 
-  length.times do |index|
+  length.times do {|index|
     if array[index] == value_to_find
     return index
+  }
 end
 nil
 
