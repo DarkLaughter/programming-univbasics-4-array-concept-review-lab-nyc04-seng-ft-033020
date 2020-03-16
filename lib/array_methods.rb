aef48455371721dcadf0cb0d2d8d36ff5558a23b
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-03-16 04:47:34 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,10 +2,9 @@ def find_element_index(array, value_to_find)
   array.length.times do |index|
     if array[index] == value_to_find
     return index
-
-end
-nil
-end
+    end
+    nil
+  end
 end
 
 def find_max_value(array)
