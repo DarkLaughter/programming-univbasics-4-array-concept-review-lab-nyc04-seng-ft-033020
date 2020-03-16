e36243d4570aa1f0035d678d772088d93650b928
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-03-16 04:32:22 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,10 +8,10 @@ end
 nil
 end
 
-def find_max_value(array)
+//def find_max_value(array)
   # Add your solution here
-end
-
-def find_min_value(array)
+//end
+//
+//def find_min_value(array)
   # Add your solution here
-end
+//end
